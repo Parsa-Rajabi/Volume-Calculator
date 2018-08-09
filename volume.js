fed32330c4eec892cab40cf162e3595caebb10bd
Parsa Rajabi <ParsaRajabiPR@gmail.com>
2018-08-08 20:03:28 -0700

    1 M & 10 M
    Fully Functioning switch between 1 M and 10 M.

--- volume.js
+++ volume.js
@@ -16,12 +16,18 @@ var width = 1;
 var height = 1;
 var length = 1;
 
-var widthSlider, heightSlider, lengthSider;
-var widthOutput, heightOutput, lengthOutput;
+var widthSlider_oneM, heightSlider_oneM, lengthSider_oneM;
+var widthOutput_oneM, heightOutput_oneM, lengthOutput_oneM;
+
+
+var widthSlider_tenM, heightSlider_tenM, lengthSider_tenM;
+var widthOutput_tenM, heightOutput_tenM, lengthOutput_tenM;
 
 var sliderX, sliderY;
 var textX, textY;
 
+var switchCheck = true;
+//true if 1 M, false if 10 M
 
 // Chrome 1+
 var isChrome = !!window.chrome && !!window.chrome.webstore;
@@ -48,8 +54,23 @@ function init() {
  */
 
 function update(event) {
-    if (gameStarted) {}
+    if (gameStarted) {
+        // if true, show 10 M stuff
+        if (!switchCheck) {
+            stage.addChild(widthSlider_tenM, heightSlider_tenM, lengthSider_tenM);
+            widthOutput_tenM.visible = heightOutput_tenM.visible = lengthOutput_tenM.visible = true;
 
+            stage.removeChild(widthSlider_oneM, heightSlider_oneM, lengthSider_oneM);
+            widthOutput_oneM.visible = heightOutput_oneM.visible = lengthOutput_oneM.visible = false;
+        } else if (switchCheck){
+            stage.removeChild(widthSlider_tenM, heightSlider_tenM, lengthSider_tenM);
+            widthOutput_tenM.visible = heightOutput_tenM.visible = lengthOutput_tenM.visible = false;
+
+            stage.addChild(widthSlider_oneM, heightSlider_oneM, lengthSider_oneM);
+            widthOutput_oneM.visible = heightOutput_oneM.visible = lengthOutput_oneM.visible = true;
+        }
+
+    }
     stage.update(event);
 }
 
@@ -68,69 +89,131 @@ function endGame() {
 function initGraphics() {
     sliderX = 610;
     sliderY = 160;
-    
+
     textX = 660;
     textY = 125;
+
+    //////////////////////////////////////////ONE M START ///////////////////////////////
+
     //    width output
     //new text(text, font, color)
-    widthOutput = new createjs.Text(width, "21px Krungthep", "#1d55a9");
-    widthOutput.x = textX;
-    widthOutput.y = textY;
-    stage.addChild(widthOutput);
-    
-    
-      //    width output
+    widthOutput_oneM = new createjs.Text(width, "21px Krungthep", "#1d55a9");
+    widthOutput_oneM.x = textX;
+    widthOutput_oneM.y = textY;
+    stage.addChild(widthOutput_oneM);
+
+
+    //    width output
     //new text(text, font, color)
-    heightOutput = new createjs.Text(width, "20px Krungthep", "#1d55a9");
-    heightOutput.x = textX;
-    heightOutput.y = textY + 80;
-    stage.addChild(heightOutput);
-    
-    
-      //    width output
+    heightOutput_oneM = new createjs.Text(height, "20px Krungthep", "#1d55a9");
+    heightOutput_oneM.x = textX;
+    heightOutput_oneM.y = textY + 80;
+    stage.addChild(heightOutput_oneM);
+
+
+    //    width output
     //new text(text, font, color)
-    lengthOutput = new createjs.Text(width, "20px Krungthep", "#1d55a9");
-    lengthOutput.x = textX;
-    lengthOutput.y = textY + 160;
-    stage.addChild(lengthOutput);
-    
-    
+    lengthOutput_oneM = new createjs.Text(length, "20px Krungthep", "#1d55a9");
+    lengthOutput_oneM.x = textX;
+    lengthOutput_oneM.y = textY + 160;
+    stage.addChild(lengthOutput_oneM);
+
 
     // new Slider(min, max, width, height)
-    widthSlider = new Slider(1, 5, 100, 30).set({
+    widthSlider_oneM = new Slider(1, 5, 100, 30).set({
         x: sliderX,
         y: sliderY,
         value: 1 //default value
     });
 
     // new Slider(min, max, width, height)
-    heightSlider = new Slider(1, 5, 100, 30).set({
+    heightSlider_oneM = new Slider(1, 5, 100, 30).set({
         x: sliderX,
         y: sliderY + 80,
         value: 1 //default value
     });
 
     // new Slider(min, max, width, height)
-    lengthSider = new Slider(1, 5, 100, 30).set({
+    lengthSider_oneM = new Slider(1, 5, 100, 30).set({
         x: sliderX,
         y: sliderY + 160,
         value: 1 //default value
     });
 
-    widthSlider.on("change", handleWidthSliderChange, this); // assign event handler to the slider (What function to call)
+    widthSlider_oneM.on("change", handleWidthSliderChange_oneM, this); // assign event handler to the slider (What function to call)
 
-    heightSlider.on("change", handleHeightSliderChange, this); // assign event handler to the slider (What function to call)
+    heightSlider_oneM.on("change", handleHeightSliderChange_oneM, this); // assign event handler to the slider (What function to call)
 
-    lengthSider.on("change", handleLengthSliderChange, this); // assign event handler to the slider (What function to call)
+    lengthSider_oneM.on("change", handleLengthSliderChange_oneM, this); // assign event handler to the slider (What function to call)
 
-    stage.addChild(widthSlider, heightSlider, lengthSider);
+    stage.addChild(widthSlider_oneM, heightSlider_oneM, lengthSider_oneM);
+
+    //////////////////////////////////////////TEN M START///////////////////////////////
+
+    //    width output
+    //new text(text, font, color)
+    widthOutput_tenM = new createjs.Text(width*10, "21px Krungthep", "#1d55a9");
+    widthOutput_tenM.x = textX;
+    widthOutput_tenM.y = textY;
+    stage.addChild(widthOutput_tenM);
 
 
-    
-    switchCM.x = switchM.x = 590;
-    switchCM.y = switchM.y = 20;
-    stage.addChild(switchCM, switchM);
-    switchCM.visible = false;
+    //    width output
+    //new text(text, font, color)
+    heightOutput_tenM = new createjs.Text(height*10, "20px Krungthep", "#1d55a9");
+    heightOutput_tenM.x = textX;
+    heightOutput_tenM.y = textY + 80;
+    stage.addChild(heightOutput_tenM);
+
+
+    //    width output
+    //new text(text, font, color)
+    lengthOutput_tenM = new createjs.Text(length*10, "20px Krungthep", "#1d55a9");
+    lengthOutput_tenM.x = textX;
+    lengthOutput_tenM.y = textY + 160;
+    stage.addChild(lengthOutput_tenM);
+
+
+    // new Slider(min, max, width, height)
+    widthSlider_tenM = new Slider(1, 5, 100, 30).set({
+        x: sliderX,
+        y: sliderY,
+        value: width //default value
+    });
+
+    // new Slider(min, max, width, height)
+    heightSlider_tenM = new Slider(1, 5, 100, 30).set({
+        x: sliderX,
+        y: sliderY + 80,
+        value: height //default value
+    });
+
+    // new Slider(min, max, width, height)
+    lengthSider_tenM = new Slider(1, 5, 100, 30).set({
+        x: sliderX,
+        y: sliderY + 160,
+        value: length //default value
+    });
+
+    widthSlider_tenM.on("change", handleWidthSliderChange_tenM, this); // assign event handler to the slider (What function to call)
+
+    heightSlider_tenM.on("change", handleHeightSliderChange_tenM, this); // assign event handler to the slider (What function to call)
+
+    lengthSider_tenM.on("change", handleLengthSliderChange_tenM, this); // assign event handler to the slider (What function to call)
+
+    //    stage.addChild(widthSlider_tenM, heightSlider_tenM, lengthSider_tenM);
+
+    //    widthSlider_tenM.visible = heightSlider_tenM.visible = lengthSider_tenM.visible = false; 
+    widthOutput_tenM.visible = heightOutput_tenM.visible = lengthOutput_tenM.visible = false;
+    //////////////////////////////////////////TEN M OVER////////////////////////////////
+
+
+
+    switchTenM.x = switchOneM.x = 590;
+    switchTenM.y = switchOneM.y = 20;
+    stage.addChild(switchTenM, switchOneM);
+    switchTenM.visible = false;
+
     initMuteUnMuteButtons();
     initListeners();
 
@@ -139,22 +222,47 @@ function initGraphics() {
     stage.update();
 }
 
-function handleWidthSliderChange(evt) {
+//////////////////////////////////////////ONE M//////////////////////////////////////////
+
+function handleWidthSliderChange_oneM(evt) {
     width = Math.round(evt.target.value);
-    widthOutput.text = width;
-//    console.log("Width: " + width);
+    widthOutput_oneM.text = width;
+    console.log("Width: " + width);
 }
 
-function handleHeightSliderChange(evt) {
-    height = Math.round(evt.target.value)
-    heightOutput.text = height;
+function handleHeightSliderChange_oneM(evt) {
+
+    height = Math.round(evt.target.value);
+    heightOutput_oneM.text = height;
     console.log("Height: " + height);
 }
 
-function handleLengthSliderChange(evt) {
+function handleLengthSliderChange_oneM(evt) {
     length = Math.round(evt.target.value)
-    lengthOutput.text = length;
+    lengthOutput_oneM.text = length;
     console.log("Length: " + length);
+}
+
+
+
+//////////////////////////////////////////TEN M//////////////////////////////////////////
+function handleWidthSliderChange_tenM(evt) {
+    width = Math.round(evt.target.value)*10;
+    widthOutput_tenM.text = width;
+    console.log("Ten Width: " + width);
+}
+
+function handleHeightSliderChange_tenM(evt) {
+
+    height = Math.round(evt.target.value)*10;
+    heightOutput_tenM.text = height;
+    console.log("Ten Height: " + height);
+}
+
+function handleLengthSliderChange_tenM(evt) {
+    length = Math.round(evt.target.value)*10;
+    lengthOutput_tenM.text = length;
+    console.log("Ten Length: " + length);
 }
 /*
  * Adds the mute and unmute buttons to the stage and defines listeners
@@ -180,23 +288,25 @@ function initMuteUnMuteButtons() {
  * Add listeners to objects.
  */
 function initListeners() {
-    
+
     //////////////ON SWITCH///////////////
-    switchCM.on("click", switchToCM);
-    switchM.on("click", switchToM);
+    switchTenM.on("click", switchToOneM);
+    switchOneM.on("click", switchToTenM);
 
 }
 
-function switchToCM(){
-    console.log("switch is to CM" );
-    switchM.visible = true;
-    switchCM.visible = false
+function switchToOneM() {
+    console.log("switch is to One M " + switchCheck);
+    switchOneM.visible = true;
+    switchTenM.visible = false
+    switchCheck = true;
 }
 
-function switchToM(){
-    console.log("switch is to M" );
-    switchCM.visible = true;
-    switchM.visible = false;
+function switchToTenM() {
+    console.log("switch is to Ten M " + switchCheck);
+    switchTenM.visible = true;
+    switchOneM.visible = false;
+    switchCheck = false;
 }
 
 //////////////////////// PRELOADJS FUNCTIONS
@@ -204,7 +314,7 @@ function switchToM(){
 // bitmap variables
 var muteButton, unmuteButton;
 var background;
-var switchCM, switchM;
+var switchTenM, switchOneM;
 
 
 /*
@@ -215,10 +325,10 @@ function setupManifest() {
         {
             src: "images/switchLeft.png",
             id: "LeftisM"
-    },{
+    }, {
             src: "images/switchRight.png",
             id: "RightisCM"
-    },{
+    }, {
             src: "images/Background.png",
             id: "background"
     }, {
@@ -249,9 +359,9 @@ function handleFileLoad(event) {
     console.log("A file has loaded of type: " + event.item.type);
     // create bitmaps of images
     if (event.item.id == "RightisCM") {
-        switchCM = new createjs.Bitmap(event.result);
+        switchTenM = new createjs.Bitmap(event.result);
     } else if (event.item.id == "LeftisM") {
-        switchM = new createjs.Bitmap(event.result);
+        switchOneM = new createjs.Bitmap(event.result);
     } else if (event.item.id == "background") {
         background = new createjs.Bitmap(event.result);
     } else if (event.item.id == "mute") {
