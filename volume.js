70cce45eaf63d652b54e50889eb4b1d931cf2a4f
Parsa Rajabi <ParsaRajabiPR@gmail.com>
2018-08-14 19:08:14 -0700

    Updated var names in for loops

--- volume.js
+++ volume.js
@@ -235,21 +235,21 @@ function updateCube() {
 	var originX = 254;
 	var originY = 458;
 	
-	for (var i = 0; i < heightSlider.value; i++) {
+	for (var i = 0; i < height; i++) {
 		var temp = Object.create(cube);
 		temp.x = originX;
 		temp.y = originY - i * cube.image.height/2;
 		stage.addChild(temp);
 	}	
 	
-	for (var j = 0; j < lengthSider.value; j++) {
+	for (var j = 0; j < length; j++) {
 		var temp = Object.create(cube);
 		temp.x = originX + j * cube.image.width/2
 		temp.y = originY - 22 * j;
 		stage.addChildAt(temp, 2);
 	}
 	
-	for (var k = 0; k < widthSlider.value; k++) {
+	for (var k = 0; k < width; k++) {
 		var temp = Object.create(cube);
 		temp.x = originX - k * cube.image.width/2
 		temp.y = originY - 22 * k;
